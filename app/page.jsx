65576e3bf0ba2ac20ed11f4ec9fcc6414a4b0ff0
Jay Mehta <23277339+jaymehta20@users.noreply.github.com>
2023-06-02 00:09:14 +0530

    fix typo

--- app/page.jsx
+++ app/page.jsx
@@ -9,7 +9,7 @@ const Home = () => {
         <span className="orange_gradient text-center"> AI-Powered Prompts</span>
       </h1>
       <p className="desc text-center">
-        Promptopia is an open-source AI prompting toll for modern world to
+        Promptopia is an open-source AI prompting tool for modern world to
         discover, create and share creative prompts
       </p>
 
